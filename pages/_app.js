c6f0b31b285d0f637b513359d65d8b8eeb3e3e9b
Kim-menu <hyunwjin@gmail.com>
2022-04-29 16:11:35 +0900

    Apply to fetch data

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import NavBar from "../components/NavBar";
 import "../styles/globals.css";
+import Layout from "../components/Layout";
 
 export default function App({Component, pageProps}) {
-    return <div>
-        <NavBar />
-        <Component {...pageProps}/>
-    </div>
+    return (
+        <Layout>
+            <Component {...pageProps}/>
+        </Layout>
+    );
 }
